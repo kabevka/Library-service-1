bf623c5bf7fc420d7f59730aab517d412139d8f8
Volodymyr95 <krokhmalyuk@live.com>
2016-03-28 00:45:38 +0300

    add Dao factory, and implemented my etity dao

---
+++ src/main/java/edu/ita/softserve/dao/impl/jpa/JpaTakenDao.java
@@ -0,0 +1,8 @@
+package edu.ita.softserve.dao.impl.jpa;
+
+import edu.ita.softserve.dao.impl.TakenDao;
+import edu.ita.softserve.entity.Taken;
+
+public class JpaTakenDao extends JpaGenericDao<Taken,Long> implements TakenDao {
+
+}
---
+++ src/main/java/edu/ita/softserve/dao/impl/jpa/JpaUserDao.java
@@ -0,0 +1,30 @@
+package edu.ita.softserve.dao.impl.jpa;
+
+import java.util.List;
+
+import edu.ita.softserve.dao.impl.UserDao;
+import edu.ita.softserve.entity.User;
+
+public class JpaUserDao extends JpaGenericDao<User, Long> implements UserDao {
+
+	public List<User> getAllDeptors() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String statisticByUser(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String statisticByAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getAvarageAgeByUser(User user) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
