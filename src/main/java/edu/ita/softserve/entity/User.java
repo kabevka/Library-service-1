1182b085fa661202eaf796d35ae6be8b78c78a3d
Volodymyr95 <krokhmalyuk@live.com>
2016-03-27 23:43:20 +0300

    add jpa dependecies

---
+++ src/main/java/edu/ita/softserve/entity/User.java
@@ -0,0 +1,14 @@
+package edu.ita.softserve.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6671319195214019822L;
+	
+	
+
+}
