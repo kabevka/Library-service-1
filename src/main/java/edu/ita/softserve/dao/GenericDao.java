cbc0af87f34f8afe978e0bc2b200e4557d705aa9
Volodymyr95 <krokhmalyuk@live.com>
2016-03-27 23:36:05 +0300

    add dao interface

---
+++ src/main/java/edu/ita/softserve/dao/GenericDao.java
@@ -0,0 +1,13 @@
+package edu.ita.softserve.dao;
+
+import java.util.List;
+
+public interface GenericDao<T,V> {
+	
+	void add(T t);
+	T findById(V id);
+	void update(T t);
+	void delete(T t);
+	List<T> getAll();
+
+}
