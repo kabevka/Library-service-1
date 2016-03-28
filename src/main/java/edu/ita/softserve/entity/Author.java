e7f56740c07748ee7b4423d326a3f51bc986fa69
Anna KObevka <kobevka.ai@gmail.com>
2016-03-28 22:41:50 +0300

    committed Author & Publisher Entities

--- src/main/java/edu/ita/softserve/entity/Author.java
+++ src/main/java/edu/ita/softserve/entity/Author.java
@@ -1,18 +1,75 @@
 package edu.ita.softserve.entity;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "authors")
-public class Author {
-    // - It's moock-Class who has this entity have to fill it.
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id")
-    private int id;
+public class Author implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column (name = "name", length = 45)
+	private String name;
+	
+	@Column (name = "surname", length = 45)
+	private String surname;
+	
+	@ManyToMany(cascade=CascadeType.ALL, targetEntity=Book.class)
+	private List<Book> bookList = new ArrayList<Book>();
+	
+	public Author() {}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public List<Book> getBookList() {
+		return bookList;
+	}
+
+	public void setBookList(List<Book> bookList) {
+		this.bookList = bookList;
+	}
+
+
+	
 }
