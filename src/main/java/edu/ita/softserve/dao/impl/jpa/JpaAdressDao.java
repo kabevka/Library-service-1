e37f654ef958071a174900268dcd260e1917e1f6
Volodymyr95 <krokhmalyuk@live.com>
2016-03-29 17:06:18 +0300

    add JpaAdressDao

---
+++ src/main/java/edu/ita/softserve/dao/impl/jpa/JpaAdressDao.java
@@ -0,0 +1,8 @@
+package edu.ita.softserve.dao.impl.jpa;
+
+import edu.ita.softserve.dao.impl.AdressDao;
+import edu.ita.softserve.entity.Adress;
+
+public class JpaAdressDao extends JpaGenericDao<Adress, Long> implements AdressDao {
+
+}
